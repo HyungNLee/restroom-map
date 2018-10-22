13c746b5bf9e61754f34d2380606a08cf54c7c9d
Hyewon Cho and Hyung Lee <hyewon@>
2018-10-22 16:39:32 -0700

    Add QFC and amazon Books.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,12 +16,26 @@ export class AppComponent {
           mapTypeId: google.maps.MapTypeId.ROADMAP
       };
       let map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-      let williamSonoma = new Place("William Sonoma", "4619 26th Ave NE, Seattle, WA 98105", "Example", false);
+
+      let williamSonoma = new Place("William Sonoma", "4619 26th Ave NE, Seattle, WA 98105", "Opening Hours", false);
       let coordinatePromise = williamSonoma.getCoordinate();
       coordinatePromise.then((coordinate: Coordinate) => {
         williamSonoma.coordinate = coordinate;
         let marker = williamSonoma.setMarker(map);
       });
 
+      let QFC = new Place("QFC", "2746 NE 45th St, Seattle, WA 98105", "Opening Hours", false);
+      coordinatePromise = QFC.getCoordinate();
+      coordinatePromise.then((coordinate: Coordinate) => {
+        QFC.coordinate = coordinate;
+        let marker = QFC.setMarker(map);
+      });
+
+      let amazonBooks = new Place("Amazon Books", "4601 26th Ave NE Seattle, WA 98105", "Opening Hours", false);
+      coordinatePromise = amazonBooks.getCoordinate();
+      coordinatePromise.then((coordinate: Coordinate) => {
+        amazonBooks.coordinate = coordinate;
+        let marker = amazonBooks.setMarker(map);
+      })
   }
 }
